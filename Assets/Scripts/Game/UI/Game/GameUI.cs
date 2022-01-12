25a512950d9d818e4c1053b2f7ada1ee4a4170b1
SasakiHayato0605 <zuomusundou685@gmail.com>
2022-01-12 12:04:10 +0900

    GamePanelの修正

--- Assets/Scripts/Game/UI/Game/GameUI.cs
+++ Assets/Scripts/Game/UI/Game/GameUI.cs
@@ -2,75 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using DG.Tweening;
 
 public class GameUI : UIWindowParent
 {
-    [SerializeField] Vector2 _setCanvasPos;
-    [SerializeField] int _maxID;
-    bool _canvasActive = true;
-    RectTransform _rect;
-
-    int _saveValue = 0;
-    int _canvasID = 0;
-
     public override void SetUp()
     {
-        _rect = GetPanel.GetComponent<RectTransform>();
-        _rect.anchoredPosition = _setCanvasPos;
-
-        Inputter.Instance.Inputs.UI.Options.started += context
-            => SetCanvas();
-
         base.SetUp();
-        SetCanvas();
     }
 
     public override void UpDate()
     {
-        if (_canvasActive)
-        {
-            Select();
-            base.UpDate();
-        }
-        else
-        {
-            CallBack(0, null);
-        }
+        base.UpDate();
     }
 
     public override void CallBack(int id, object[] data)
     {
         base.CallBack(id, data);
-    }
-
-    void Select()
-    {
-        Vector2 get = (Vector2)Inputter.GetValue(InputType.Select);
-        if ((int)get.y != _saveValue)
-        {
-            _saveValue = (int)get.y;
-            _canvasID += _saveValue;
-            if (_canvasID > _maxID) _canvasID = _maxID;
-            else if (_canvasID < 0) _canvasID = 0;
-        }
-    }
-
-    void SetCanvas()
-    {
-        Sounds.SoundMaster.Request(null, "Click", 4);
-
-        if (!_canvasActive)
-        {
-            _rect.DOAnchorPosX(0, 0.2f)
-                .SetEase(Ease.Linear)
-                .OnComplete(() => _canvasActive = true);
-        }
-        else
-        {
-            _rect.DOAnchorPosX(_setCanvasPos.x, 0.2f)
-                .SetEase(Ease.Linear)
-                .OnComplete(() => _canvasActive = false);
-        }
     }
 }
--- Assets/Scripts/Game/UI/Game/MenuPanel.cs
+++ Assets/Scripts/Game/UI/Game/MenuPanel.cs
@@ -2,24 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class MenuPanel : UIWindowParent.UIWindowChild
 {
     [SerializeField] string _name;
+    [SerializeField] Vector2 _setPos;
+
     Image _panel;
     RectTransform _rect;
     Vector2 _saveVec = Vector2.zero;
+
+    bool _isActive = false;
 
     public override void SetUp()
     {
         _panel = ParentPanel.gameObject.transform.Find(_name).GetComponent<Image>();
         _rect = _panel.GetComponent<RectTransform>();
-        _saveVec = _rect.position;
+        _saveVec = _rect.anchoredPosition;
+        _rect.anchoredPosition = _setPos;
+
+        Inputter.Instance.Inputs.UI.Options.started += context => SetCanvas();
+    }
+
+    void SetCanvas()
+    {
+        if (_isActive)
+        {
+            _rect.DOAnchorPosX(_setPos.x, 0.2f)
+                .SetEase(Ease.Linear)
+                .OnComplete(() => _isActive = false);
+        }
+        else
+        {
+            _rect.DOAnchorPosX(_saveVec.x, 0.2f)
+                .SetEase(Ease.Linear)
+                .OnComplete(() => _isActive = true);
+        }
     }
 
     public override void UpDate()
     {
-        
+        if (_isActive)
+        {
+            Select();
+        }
+    }
+
+    void Select()
+    {
+
     }
 
     public override void CallBack(object[] data)
