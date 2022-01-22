f22bdaa572d670153a1367bc0f9c08838aa4de62
SasakiHayato0605 <zuomusundou685@gmail.com>
2022-01-22 13:20:36 +0900

    Bossの作成、過程

--- Assets/Scripts/Systems/BehaviorTree/Action/RunStateMachine.cs
+++ Assets/Scripts/Systems/BehaviorTree/Action/RunStateMachine.cs
@@ -6,12 +6,13 @@ using BehaviorAI;
 public class RunStateMachine : IAction
 {
     [SerializeField] StateMachine _state;
+    [SerializeField] bool _isRun;
     bool _check = false;
     
     public GameObject Target { private get; set; }
     public void Execute()
     {
-        _state.RunRequest(true);
+        _state.RunRequest(_isRun);
         _state.Base();
 
         _check = true;
