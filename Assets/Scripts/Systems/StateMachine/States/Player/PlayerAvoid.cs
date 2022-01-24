96164ac90bf92e48b749a5f9313f49e0e2ef720a
SasakiHayato0605 <zuomusundou685@gmail.com>
2022-01-24 17:48:35 +0900

    回避時の条件を追加

--- Assets/Scripts/Systems/StateMachine/States/Player/PlayerAvoid.cs
+++ Assets/Scripts/Systems/StateMachine/States/Player/PlayerAvoid.cs
@@ -4,12 +4,15 @@ using Sounds;
 public class PlayerAvoid : StateMachine.State
 {
     [SerializeField] float _avoidTime;
-    [SerializeField] float _speed;
+    [SerializeField] float _defSpeed;
+    [SerializeField] float _isLockOnSpeed;
+    [SerializeField] float _targetDist;
 
     GameObject _mainCm;
     Vector2 _input = Vector2.zero;
     
     float _currentTime;
+    float _setSpeed;
 
     public override void Entry(StateMachine.StateType beforeType)
     {
@@ -19,6 +22,17 @@ public class PlayerAvoid : StateMachine.State
         _input = (Vector2)Inputter.GetValue(InputType.PlayerMove);
         _input = _input.normalized;
         SoundMaster.Request(Target.transform, "Avoid", 0);
+        GameObject t = GameManager.Instance.LockonTarget;
+        if (t != null)
+        {
+            float dist = Vector3.Distance(t.transform.position, Target.transform.position);
+            if (dist < _targetDist) _setSpeed = _isLockOnSpeed;
+            else _setSpeed = _defSpeed;
+        }
+        else
+        {
+            _setSpeed = _defSpeed;
+        }
     }
 
     public override void Run(out Vector3 move)
@@ -27,8 +41,8 @@ public class PlayerAvoid : StateMachine.State
         if (_input == Vector2.zero) _input = Vector2.up * -1;
         _currentTime += Time.deltaTime;
         
-        Vector3 forward = _mainCm.transform.forward * _input.y * _speed;
-        Vector3 right = _mainCm.transform.right * _input.x * _speed;
+        Vector3 forward = _mainCm.transform.forward * _input.y * _setSpeed;
+        Vector3 right = _mainCm.transform.right * _input.x * _setSpeed;
 
         move = new Vector3(forward.x + right.x, 1, right.z + forward.z);
     }
