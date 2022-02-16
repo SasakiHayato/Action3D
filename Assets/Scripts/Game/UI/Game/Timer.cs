d056bb9400318f732831705420189c6e4abdaf31
SasakiHayato0605 <zuomusundou685@gmail.com>
2022-02-16 14:54:57 +0900

    Timerの修正

--- Assets/Scripts/Game/UI/Game/Timer.cs
+++ Assets/Scripts/Game/UI/Game/Timer.cs
@@ -13,8 +13,11 @@ public class Timer : UIWindowParent.UIWindowChild
 
     public override void UpDate()
     {
-        float time = GameManager.Instance.GetCurrentTime;
-        _txt.text = $"Time ; {time.ToString("00:00.00")}";
+        float totaltime = GameManager.Instance.GetCurrentTime;
+        int minutes = (int)totaltime / 60;
+        float second = totaltime - minutes * 60;
+
+        _txt.text = $"Time ; {minutes.ToString("00")}:{second.ToString($"00.00")}";
     }
 
     public override void CallBack(object[] data)
