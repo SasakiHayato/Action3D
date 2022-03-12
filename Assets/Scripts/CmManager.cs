5aa99b24839e4ccc50529a35d9209dad1f69cf63
SasakiHayato0605 <zuomusundou685@gmail.com>
2022-03-13 01:34:56 +0900

    Camera制御の作成、過程

--- Assets/Scripts/CmManager.cs
+++ Assets/Scripts/CmManager.cs
@@ -5,13 +5,18 @@ public class CmManager : MonoBehaviour
     [SerializeField] Transform _user;
     [SerializeField] Transform _targetCm;
     [SerializeField] Vector3 _offSetPos;
-    [SerializeField] float _sensitivity = 0.5f;
-    [SerializeField] float _rotateDelay;
+    [SerializeField] float _sensitivityX = 0.5f;
+    [SerializeField] float _sensitivityY = 0.5f;
+    [SerializeField] float _viewDelay;
+    [SerializeField] float _dead = 0.1f;
 
     float _rotateTimer;
-    float _angle;
+    float _horizontalAngle;
+    float _verticleAngle;
 
     float _dist;
+    float _savePosY;
+    Vector2 _saveHorizontalPos;
 
     void Start()
     {
@@ -21,43 +26,69 @@ public class CmManager : MonoBehaviour
 
         _targetCm.position = _user.position + _offSetPos;
         _dist = Vector3.Distance(_user.position, _targetCm.position);
-        Debug.Log(_dist);
+
+        _saveHorizontalPos = new Vector2(_targetCm.position.x, _targetCm.position.z);
+        _horizontalAngle = Mathf.Atan2(_saveHorizontalPos.y, _saveHorizontalPos.x) * Mathf.Rad2Deg - 90;
+
+        _savePosY = _targetCm.position.y;
     }
 
     void Update()
     {
-        RotateX();
+        Vector2 input = (Vector2)Inputter.GetValue(InputType.CmMove);
+        
+        Vector2 pos = HorizontalPos(input.normalized.x);
+        float y = VerticlePos(input.normalized.y);
+
+        _targetCm.position = new Vector3(pos.x, y, pos.y);
+
         View();
     }
 
-    void RotateX()
+    Vector2 HorizontalPos(float x)
     {
-        Vector2 input = (Vector2)Inputter.GetValue(InputType.CmMove);
-        
-        if (input.x > 0) _angle++;
-        else if (input.x < 0) _angle--;
-        
-        float angle = _angle * _sensitivity * -1;
+        float s = float.Parse(x.ToString("0.0"));
+        if (Mathf.Abs(s * s) < _dead) return _saveHorizontalPos;
 
-        if (angle >= 360 || angle <= -360) angle = 0;
-        Debug.Log(angle);
+        if (x > 0) _horizontalAngle++;
+        else if (x < 0) _horizontalAngle--;
 
+        float angle = _horizontalAngle * _sensitivityX;
+
+        if (angle >= 360 || angle <= -360)
+        {
+            _horizontalAngle = 0;
+            angle = 0;
+        }
+        
         float rad = angle * Mathf.Deg2Rad;
-        Vector3 pos = new Vector3(Mathf.Sin(rad), 0, Mathf.Cos(rad)) * _dist;
+        Vector3 pos = new Vector3(Mathf.Cos(rad), 0, Mathf.Sin(rad)) * _dist;
 
         Vector3 setPos = pos + _user.position;
-        setPos.y = _offSetPos.y;
-        _targetCm.position = setPos;
+
+        _saveHorizontalPos = new Vector2(setPos.x, setPos.z);
+
+        return _saveHorizontalPos;
     }
 
-    void RotateY()
+    float VerticlePos(float y)
     {
+        float s = float.Parse(y.ToString("0.0"));
+        if (Mathf.Abs(s * s) <  _dead) return _savePosY;
 
+        if (y > 0) _verticleAngle++;
+        else if (y < 0) _verticleAngle--;
+
+        float angle = _verticleAngle * _sensitivityY * -1;
+
+        float rad = angle * Mathf.Deg2Rad;
+        _savePosY = _offSetPos.y + Mathf.Sin(rad) * _dist;
+        return _savePosY;
     }
 
     void View()
     {
-        _rotateTimer += Time.deltaTime / _rotateDelay;
+        _rotateTimer += Time.deltaTime / _viewDelay;
 
         Vector3 dir = _user.position - _targetCm.position;
         Quaternion q = Quaternion.LookRotation(dir.normalized);
